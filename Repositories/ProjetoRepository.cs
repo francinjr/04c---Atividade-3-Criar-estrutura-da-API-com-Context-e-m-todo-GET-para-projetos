826699bbdfccd4c6ff98cc2cdee45366a696b64e
francinjr <francinaldomanoel135@gmail.com>
2023-04-28 19:57:43 -0300

    Atv 4

--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -1,6 +1,6 @@
+using System;
 using Exo.WebApi.Contexts;
 using Exo.WebApi.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,6 +16,39 @@ namespace Exo.WebApi.Repositories
         public List<Projeto> Listar()
         {
             return _context.Projetos.ToList();
+        }
+
+        // código novo
+
+        public void Cadastrar(Projeto projeto)
+        {
+            _context.Projetos.Add(projeto);
+            _context.SaveChanges();
+        }
+
+        public Projeto BuscarPorId(int id)
+        {
+            return _context.Projetos.Find(id);
+        }
+
+        public void Atualizar(int id, Projeto projeto)
+        {
+            Projeto projetoBuscado = _context.Projetos.Find(id);
+            if(projetoBuscado != null)
+            {
+                projetoBuscado.NomeDoProjeto = projeto.NomeDoProjeto;
+                projetoBuscado.Area = projeto.Area;
+                projetoBuscado.Status = projeto.Status;
+            }
+            _context.Projetos.Update(projetoBuscado);
+            _context.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Projeto projetoBuscado = _context.Projetos.Find(id);
+            _context.Projetos.Remove(projetoBuscado);
+            _context.SaveChanges();
         }
     }
 }
